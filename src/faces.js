0f846965211e512295199ffb220e63da6a46edae
Kimmo Puputti <kpuputti@gmail.com>
2012-05-01 16:57:30 +0300

    Implement a set of matrix handling functions.

--- src/faces.js
+++ src/faces.js
@@ -34,6 +34,13 @@
         req.send();
     };
 
+    // Calculate the sum of the item in the given array
+    var sum = function (arr) {
+        return arr.reduce(function (a, b) {
+            return a + b;
+        });
+    };
+
     // Flatten a matrix
     var flatten = function (arr) {
         return arr.reduce(function (a, b) {
@@ -48,16 +55,24 @@
         }) / arr.length;
     };
 
+    // Create a matrix with the given dimensions with zero in each
+    // cell
+    var getZeroMatrix = function (rows, cols) {
+        var matrix = [];
+        for (var i = 0; i < rows; ++i) {
+            matrix[i] = [];
+            for (var j = 0; j < cols; ++j) {
+                matrix[i][j] = 0;
+            }
+        }
+        return matrix;
+    };
+
     // Transform a matrix
     var transform = function (matrix) {
-        if (matrix.length === 0) {
-            return [];
-        }
         // init new matrix with as many rows as there are columns in
         // the given matrix
-        var transformed = matrix[0].map(function () {
-            return [];
-        });
+        var transformed = getZeroMatrix(matrix[0].length, matrix.length);
 
         // Transform the matrix values
         matrix.forEach(function (row, rowIndex) {
@@ -67,6 +82,42 @@
         });
 
         return transformed;
+    };
+
+    // Calculate the dot product of the given vectors
+    var dotProduct = function (vector1, vector2) {
+        if (vector1.length !== vector2.length) {
+            throw new Error('Cannot calculate dot product with vectors of different length.');
+        }
+        return sum(vector1.map(function (val, index) {
+            return val * vector2[index];
+        }));
+    };
+
+    // Get the nth column of the given matrix
+    var getMatrixColumn = function (matrix, n) {
+        var column = [];
+        matrix.forEach(function (row) {
+            column.push(row[n]);
+        });
+        return column;
+    };
+
+    // Multiply the given matrices
+    var multiply = function (matrix1, matrix2) {
+        if (matrix1.length === 0 || matrix1[0].length !== matrix2.length) {
+            throw new Error('Cannot multiply matrices with the given dimensions.');
+        }
+        var result = getZeroMatrix(matrix1.length, matrix2[0].length);
+
+        var row;
+        for (var i = 0, len1 = result.length; i < len1; ++i) {
+            for (var j = 0, len2 = result[i].length; j < len2; ++j) {
+                result[i][j] = dotProduct(matrix1[i], getMatrixColumn(matrix2, j));
+            }
+        }
+
+        return result;
     };
 
     // Constructor function for the Faces prototype

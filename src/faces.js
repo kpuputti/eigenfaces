9433953cad488dd776be78ec7df05ebb42cc42f5
Kimmo Puputti <kpuputti@gmail.com>
2012-05-01 08:21:56 +0300

    Add CSV data fetching.

--- src/faces.js
+++ src/faces.js
@@ -1,7 +1,7 @@
 /*jslint white: true, devel: true, onevar: false, undef: true, nomen: false,
   regexp: true, plusplus: false, bitwise: true, newcap: true, maxerr: 50,
   indent: 4 */
-/*global window: false, document: false  */
+/*global window: false, document: false, XMLHttpRequest: false  */
 
 (function () {
 
@@ -13,10 +13,47 @@
         }
     };
 
-    var Faces = function (canvas) {
-        log('initialize Faces instance with canvas:', canvas);
+    var getXHR = function () {
+        return new XMLHttpRequest();
+    };
+
+    var fetch = function (url, callback) {
+        log('fetching data from url:', url);
+        var req = getXHR();
+        req.open('GET', url, false);
+        req.onreadystatechange = function () {
+            if (req.readyState === 4 && req.status === 200) {
+                callback(req.responseText);
+            }
+        };
+        req.send();
+    };
+
+    var Faces = function (canvas, csvURL) {
+        log('initialize Faces instance with canvas:', canvas,
+            'and CSV URL:', csvURL);
         this.canvas = canvas;
         this.context = canvas.getContext('2d');
+        this.csvURL = csvURL;
+        this.init();
+    };
+
+    Faces.prototype.init = function () {
+        var that = this;
+        this.fetchCSV(function (response) {
+            var parsed = that.parse(response);
+        });
+    };
+
+    Faces.prototype.fetchCSV = function (callback) {
+        fetch(this.csvURL, function (response) {
+            log('received response:', response.substr(0, 50) + '...');
+            callback(response);
+        });
+    };
+
+    Faces.prototype.parse = function (csvText) {
+        log('parsing CSV text with length:', csvText.length);
     };
 
     window.Faces = Faces;

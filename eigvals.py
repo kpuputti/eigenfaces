e0d83f5636c10c6b2eaa43c04874bf074584a210
Kimmo Puputti <kpuputti@gmail.com>
2012-05-06 22:13:38 +0300

    Save proper eigenvalues and eigenvectors.

--- eigvals.py
+++ eigvals.py
@@ -1,31 +1,26 @@
+import itertools
 import json
 import numpy
+import operator
 import sys
 
 
-SIDE = 19
-
-
-def normalize(eigvals):
-    minval = min(eigvals)
-    maxval = max(eigvals)
-    diff = maxval - minval
-    return numpy.array([(e - minval) / diff * 255 for e in eigvals])
-
-
 def eigvals_json(data):
-    arr = numpy.array(data)
-    eigvals = numpy.linalg.eigvals(arr).real
-    vals = normalize(eigvals)
-    return vals.reshape(SIDE, SIDE).tolist()
+    eigvals, eigvectors = numpy.linalg.eig(numpy.array(data))
+    eigdata = [(e[0], e[1]) for e in
+               itertools.izip(eigvals.real, eigvectors.real)]
+    eigdata.sort(key=operator.itemgetter(0), reverse=True)
+    return {
+        'eigenvalues': [e[0] for e in eigdata],
+        'eigenvectors': [e[1].tolist() for e in eigdata],
+    }
 
 
 def main(args):
     if len(args) != 1:
         sys.stderr.write('bad arguments\n')
         return 1
-    ifile = args[0]
-    data = json.loads(open(ifile).read())
+    data = json.loads(open(args[0]).read())
     print json.dumps(eigvals_json(data))
 
 
